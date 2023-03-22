45cbdebe06a2b04cbe117e521f2e09311f553754
Manish Lakhara <manishlakhara60402001@gmail.com>
2023-03-22 17:13:07 +0530

    Seeding

--- database/factories/MissionFactory.php
+++ database/factories/MissionFactory.php
@@ -19,20 +19,21 @@ class MissionFactory extends Factory
      */
     public function definition(): array
     {
-        $countries = Country::all()->pluck('country_id')->toArray();
-        $country = fake()->randomElement($countries);
-        $cities = City::where('country_id',$country)
-                        ->pluck('city_id')
-                        ->toArray();
-
+        // $countries = Country::all()->pluck('country_id')->toArray();
+        // $country = fake()->randomElement($countries);
+        // $cities = City::where('country_id',$country)
+        //                 ->pluck('city_id')
+        //                 ->toArray();
+        //$countries = Country::where('name','=','India')->pluck('country_id')->toArray();
+        $cities = City::where('country_id','=',102)->pluck('city_id')->toArray();
         return [
             'theme_id' => $this->faker->numberBetween(1,10),
-            'title' => $this->faker->words(4,true),
-            'short_description' => $this->faker->words(10,true),
+            'title' => $this->faker->words(5,true),
+            'short_description' => $this->faker->words(20,true),
             'description' => $this->faker->sentence(3),
             'mission_type' => $this->faker->randomElement(['GOAL','TIME']),
             'status'=> $this->faker->numberBetween(0,1),
-            'country_id'=> $country,
+            'country_id'=> '102',
             'city_id' => $this->faker->randomElement($cities),
             'start_date' => $this->faker->date(),
             'end_date' => $this->faker->date(),
