2a168d065c03a3b088b19a6e8b77964f823e88ef
RutvikKansagara <rutvikkansagara7852@gmail.com>
2023-03-24 15:12:05 +0530

    24-03

--- app/Http/Controllers/MissionDetailController.php
+++ app/Http/Controllers/MissionDetailController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Mission;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class MissionDetailController extends Controller
 {
     public function main($mission_id){
+        $user=Auth::user();
         $mission = Mission::where('mission_id',$mission_id)
                             ->get()[0];
-        return view('mission',compact('mission'));
+        return view('mission',compact('mission','user'));
     }
 }
