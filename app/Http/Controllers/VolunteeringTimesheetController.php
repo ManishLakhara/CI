952c4ba9089cbde3844021b7a43000b9ca0ddffd
RutvikKansagara <rutvikkansagara7852@gmail.com>
2023-03-27 18:41:55 +0530

    Done

---
+++ app/Http/Controllers/VolunteeringTimesheetController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use App\Models\TimeSheet;
+
+class VolunteeringTimesheetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $user = Auth::user();
+
+        $timesheets = Timesheet::all();
+
+
+        return view('volunteeringtimesheet.index', compact('user', 'timesheets'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): Response
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id): Response
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): RedirectResponse
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): RedirectResponse
+    {
+        //
+    }
+}
---
+++ resources/views/volunteeringtimesheet/index.blade.php
@@ -0,0 +1,287 @@
+@extends('layouts.app')
+
+@section('title')
+    volunteering timesheet
+@endsection
+
+@section('content')
+    <div class="container mt-5 pt-5">
+        <h2>Volunteering Timesheet</h2>
+
+        <div class="row">
+            <div class="col-lg-6 border mt-3">
+                <div class="mt-4">
+                    <span>Volunteering Hours</span>
+                    <button type="button" class="btn btn-outline-warning float-end me-3 mb-3 rounded-pill" data-toggle="modal"
+                        data-target="#volunteerhourModal"><i class="fas fa-plus"></i>
+                        Add
+                    </button>
+
+                </div>
+
+                <table class="table table-borderless table-responsive">
+
+
+                    <thead>
+                        <tr>
+                            <th>Mission</th>
+                            <th>Date</th>
+                            <th>Hours</th>
+                            <th>Minutes</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($timesheets as $timesheet)
+                            <tr>
+                                <td>{{ $timesheet->mission->title }}</td>
+                                <td>{{ $timesheet->date_volunteered }}</td>
+                                <td>{{ date('H', strtotime($timesheet->time)) }}</td>
+                                <td>{{ date('i', strtotime($timesheet->time)) }}</td>
+                                <td>
+
+                                    <a class="btn btn-white">
+                                        <i class="far fa-edit" style="color: orange;"></i>
+                                    </a>
+                                    <button type="button" class="btn btn-white">
+                                        <img src="Images/bin.png" alt="delete">
+                                    </button>
+
+
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+
+                <div class="modal fade" id="volunteerhourModal" tabindex="-1" aria-labelledby="volunteerhourModalLabel"
+                    aria-hidden="true" role="dialog">
+                    <div class="modal-dialog  modal-xl modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header" style="border-bottom: none;">
+                                <h5 class="modal-title" id="volunteerModalLabel">Please input below Volunteering Hours
+                                </h5>
+                                {{-- <button type="button" class="close" data-dismiss="modal" aria-label="Close"
+                                    style="border:none;background:none">
+                                    <span aria-hidden="true">&times;</span>
+                                </button> --}}
+                                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form>
+                                    <div class="form-group">
+                                        <label for="missionSelect">Mission</label>
+                                        <select class="form-control" id="missionSelect">
+                                            <option>Option 1</option>
+                                            <option>Option 2</option>
+                                            <option>Option 3</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group  mt-3">
+                                        <label for="dateVolunteered">Date Volunteered</label>
+                                        <input type="date" class="form-control" id="dateVolunteered">
+                                    </div>
+                                    <div class="form-group  mt-3">
+                                        <label for="hoursVolunteered">Hours</label>
+                                        <input type="number" class="form-control" id="hoursVolunteered"
+                                            placeholder="Enter Spent Hours">
+                                    </div>
+                                    <div class="form-group  mt-3">
+                                        <label for="minutesVolunteered">Minutes</label>
+                                        <input type="number" class="form-control" id="minutesVolunteered"
+                                            placeholder="Enter Spent Minutes" max="60">
+                                    </div>
+                                    <div class="form-group  mt-3">
+                                        <label for="messageTextarea">Message</label>
+                                        <textarea class="form-control" id="messageTextarea" rows="3" placeholder="Enter your message"></textarea>
+                                    </div>
+
+                                    <div class="container">
+                                        <div class=" d-flex mt-3 justify-content-end">
+                                            <button type="button" class="btn px-4 btn-outline-secondary rounded-pill"
+                                                data-dismiss="modal">Cancel</button>&nbsp;&nbsp;
+                                            <button type="submit" class="btn px-4 btn-outline-warning rounded-pill">
+                                                Submit</button>
+                                        </div>
+                                    </div>
+
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col-lg-5 border ms-5 mt-3">
+                <div class="mt-4">
+                    <span>Volunteering Goal</span>
+                    <button type="button" class="btn  btn-outline-warning float-end me-3 mb-3 rounded-pill"
+                        data-toggle="modal" data-target="#volunteergoalModal"><i class="fas fa-plus"></i>
+                        Add
+                    </button>
+                </div>
+
+                {{-- <table class="table table-borderless table-responsive">
+                    <thead>
+                        <tr>
+                            <th>Mission</th>
+                            <th>Date</th>
+                            <th>Action</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Environment</td>
+                            <td>23-02-2013</td>
+                            <td>13</td>
+
+                            <td>
+
+                                <a class="btn btn-white">
+                                    <i class="far fa-edit" style="color: orange;"></i>
+                                </a>
+                                <button type="button" class="btn btn-white">
+                                    <img src="Images/bin.png" alt="delete">
+                                </button>
+
+
+                            </td>
+
+
+
+
+                        </tr> --}}
+                {{-- @foreach ($data as $d)
+                            <tr>
+                                <td>{{ $d->title }}</td>
+                                <td>
+                                    @if ($d->status)
+                                        <div class="h5 text-success">
+                                            Active
+                                        </div>
+                                    @else
+                                        <div class="h5 text-danger">
+                                            Inactive
+                                        </div>
+                                    @endif
+                                </td>
+                                <td>
+
+                                    <a class="btn btn-white" href="{{ route('cmspage.edit', $d->cms_page_id) }}">
+                                        <img src="Images/edit.png" height="22px" width="22px" alt="edit">
+                                    </a>
+                                    <button type="button" data-toggle="modal"
+                                        data-target="#deleteModal-{{ $d->cms_page_id }}" class="btn btn-white">
+                                        <img src="Images/bin.png" alt="delete">
+                                    </button>
+                                    @include('admin.components.deleteModal', [
+                                        'id' => $d->cms_page_id,
+                                        'form_action' => 'cmspage.destroy',
+                                    ])
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach --}}
+                {{-- </tbody>
+                </table> --}}
+
+
+                <table class="table table-borderless table-responsive">
+
+
+                    <thead>
+                        <tr>
+                            <th>Mission</th>
+                            <th>Date</th>
+                            <th>Action</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        @foreach ($timesheets as $timesheet)
+                            <tr>
+                                <td>{{ $timesheet->mission->title }}</td>
+                                <td>{{ $timesheet->date_volunteered }}</td>
+                                <td>{{ $timesheet->action }}</td>
+
+                                <td>
+
+                                    <a class="btn btn-white">
+                                        <i class="far fa-edit" style="color: orange;"></i>
+                                    </a>
+                                    <button type="button" class="btn btn-white">
+                                        <img src="Images/bin.png" alt="delete">
+                                    </button>
+
+
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+
+                <div class="modal fade" id="volunteergoalModal" tabindex="-1" role="dialog"
+                    aria-labelledby="volunteerModalLabel" aria-hidden="true">
+                    <div class="modal-dialog  modal-xl modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header" style="border-bottom: none;">
+                                <h5 class="modal-title" id="volunteerModalLabel">Please input below Volunteering Goal</h5>
+                                {{-- <button type="button" class="close" data-dismiss="modal" aria-label="Close"
+                                    style="border:none;background:none">
+                                    <span aria-hidden="true">&times;</span>
+                                </button> --}}
+                                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form>
+                                    <div class="form-group">
+                                        <label for="missionDropdown">Mission</label>
+                                        <select class="form-control" id="missionDropdown">
+                                            <option>Option 1</option>
+                                            <option>Option 2</option>
+                                            <option>Option 3</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group mt-3">
+                                        <label for="action">Action</label>
+                                        <input type="text" class="form-control" id="action"
+                                            placeholder="Enter Actions">
+                                    </div>
+                                    <div class="form-group mt-3">
+                                        <label for="dateVolunteered">Date Volunteered</label>
+                                        <input type="date" class="form-control" id="dateVolunteered">
+                                    </div>
+                                    <div class="form-group mt-3">
+                                        <label for="message">Message</label>
+                                        <textarea class="form-control" id="message" rows="3" placeholder="Enter your message"></textarea>
+                                    </div>
+
+                                    <div class="container">
+                                        <div class=" d-flex mt-3 justify-content-end">
+                                            <button type="button" class="btn px-4 btn-outline-secondary rounded-pill"
+                                                data-dismiss="modal">Cancel</button>&nbsp;&nbsp;
+                                            <button type="submit" class="btn px-4 btn-outline-warning rounded-pill">
+                                                Submit</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+            </div>
+
+        </div>
+    </div>
+@endsection
