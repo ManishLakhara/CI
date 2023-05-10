0de94cafc303b5ac74dabc2346ab1460dfa9af87
Manish Lakhara <manishlakhara60402001@gmail.com>
2023-05-10 12:02:28 +0530

    Added seeding

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+<div class="container-fluid">
+    <div class="text-center">
+        <img src={{asset('Images/404-Page-image.png')}} alt="Not Found" class="img-fluid" aria-label="NotFoundImages" >
+    </div>
+</div>
